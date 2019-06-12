c93d741f353ab04edd30e47999c5c0d5ce240514
ljh <1145336911@qq.com>
2019-06-12 22:14:59 +0800

    林建翰-项目发布

---
+++ xxqg/src/xxqg/Test.java
@@ -0,0 +1,12 @@
+package xxqg;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("学习强国");
+		System.out.println("林建翰");
+		System.out.println("发布项目");
+	}
+
+}
